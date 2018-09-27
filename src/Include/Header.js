df448cc10391d340fb46adbf4e6f17ada57cbc2c
Last Kwechete <kwechetel@hotmail.com>
2018-09-27 15:50:04 +0200

    update 09/27/18 15:49

--- src/Include/Header.js
+++ src/Include/Header.js
@@ -2,9 +2,20 @@ import React from 'react';
 import "./Header.css";
 
 const Header = (props) => {
+    const style = {
+        height: "100%",
+        paddingRight: "2vh"
+    }
     return (
         <div className="Header">
-            <nav></nav>
+            <nav>
+                <img id="logo" src="klast-logo.svg" />
+
+                <div style={style} className="pull-right">
+                    <button>Log In</button>
+                    <button>Sign Up</button>
+                </div>
+            </nav>
         </div>
     )
 }
