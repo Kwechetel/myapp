bfc7793a331f52a5e9bed3bd1120aaddcaca63d0
Last Kwechete <kwechetel@hotmail.com>
2018-09-27 15:10:56 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,69 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Include/Header';
+import Header from './Include/Header';
+import Footer from './Include/Footer';
 
 class App extends Component {
-  state = {
-    persons: [
-      {name: "Last", age: "29"},
-      {name: "Rax", age: "28"},
-      {name: "Chris", age: "25"}
-    ]
-  }
-
-  switchNameEvent = (newName) => {
-    // DONT DO THIS! this.state.persons[0].name = "Last Kwechete";
-    this.setState({
-      persons: [
-        {name: newName, age: "29"},
-        {name: "Rax", age: "28"},
-        {name: "Chris", age: "65"}
-      ]
-    })
-  }
-
-  onChangeNameEvent = (event) => {
-    this.setState({
-      persons: [
-        {name: "Last", age: "29"},
-        {name: event.target.value, age: "28"},
-        {name: "Chris", age: "65"}
-      ]
-    })
-  }
-
+  
   render() {
-    const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer"
-    }
+    
     return (
       <div className="App">
-        <h1>Hi Last, I'm a react app</h1>
-        <p>This is really working!</p>
-        <button style={style} onClick={() => this.switchNameEvent("Chaps")}>Switch Name</button>
-
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age}
-        click={this.switchNameEvent.bind(this, "Panashe")}>My hobbies: Coding</Person>
-
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        onchanged={this.onChangeNameEvent} />
-
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age} />
+        <Header />
+        <Footer />
       </div>
     );
 
-    //return React.createElement('div',{className: 'App'}, React.createElement('h1',null, 'Hello Last. How\'re doing there????'));
-  }
+    }
 }
 
 export default App;
--- src/Include/Header.js
+++ src/Include/Header.js
@@ -3,10 +3,8 @@ import "./Header.css";
 
 const Header = (props) => {
     return (
-        <div className="Person">
-            <p onClick={props.click}>I'm a {props.name} and I am {props.age} old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.onchanged}  value={props.name}/>
+        <div className="Header">
+            <nav></nav>
         </div>
     )
 }
