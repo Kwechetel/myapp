fd68bca7da725b522373bfb4c8f1ac2862392a7c
Last Kwechete <kwechetel@hotmail.com>
2018-09-28 14:34:35 +0200

    update

--- src/Include/Footer.js
+++ src/Include/Footer.js
@@ -4,17 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="Footer">
-            <form name="contact" netlify>
-                <p>
-                    <label>Name <input type="text" name="name" /></label>
-                </p>
-                <p>
-                    <label>Email <input type="email" name="email" /></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
-            </form>
+            
         </div>
     );
 }
