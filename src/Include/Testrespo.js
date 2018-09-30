75d378a7a7d867147f70df495392e520bb400c57
Last Kwechete <kwechetel@hotmail.com>
2018-09-30 12:12:47 +0200

    update

--- src/Include/Testrespo.js
+++ src/Include/Testrespo.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 export default class PersonList extends React.Component {
 
   componentDidMount() {
-    axios.get(`http://server.klast.academy/index.php`)
+    axios.get(`https://server.klast.academy/index.php`)
       .then(res => {
         const persons = res.data;
         console.log(persons);
