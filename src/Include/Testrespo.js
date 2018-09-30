d1b713553fb64617e7f318c28eac38c6018ae9b1
Last Kwechete <kwechetel@hotmail.com>
2018-09-30 16:46:21 +0200

    update

--- src/Include/Testrespo.js
+++ src/Include/Testrespo.js
@@ -3,19 +3,23 @@ import React from 'react';
 import axios from 'axios';
 
 export default class PersonList extends React.Component {
-
-  componentDidMount() {
-    axios.get(`https://server.klast.academy/index.php`)
-      .then(res => {
-        const persons = res.data;
-        console.log(persons);
-      })
-  }
+    state = {
+        person:[{}]
+    }
+    componentDidMount() {
+        
+        axios.get(`https://server.klast.academy/index.php`)
+        .then(res => {
+            const persons = res.data;
+            this.setState({person: persons});
+            console.log(persons);
+        })
+    }
 
   render() {
     return (
       <ul>
-          
+          {this.state.person.map( (person, index) => <React.Fragment><li key={index}>{person.name}</li></React.Fragment>)}
       </ul>
     )
   }
