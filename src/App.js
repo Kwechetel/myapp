18a35ff5caf3bcab5cca6b81bed5ecc66018dd2e
Last Kwechete <kwechetel@hotmail.com>
2018-10-03 14:48:20 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Testrespo from './Components/Testrespo';
+import Codemirror from './Components/Codemirror';
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 
 class App extends Component {
@@ -12,12 +15,39 @@ class App extends Component {
   render() {
     
     return (
-      <div className="App">
-        <Header user={Math.floor(Math.random() * 2)} />
-        <Testrespo />
-        <Footer />
 
-      </div>
+      <Router>
+        <div className="App">
+          <Header user={Math.floor(Math.random() * 2)} />
+
+          <Link to='/'> <button>Home</button> </Link>
+          <Link to='/about'> <button>About</button> </Link>
+          <Link to='/course'> <button>Learn</button> </Link>
+          <Route path='/' exact strict render={
+            () => {
+              return (
+                <h1>Welcome Home</h1>
+              );
+            }
+          }/>
+          <Route path='/about' exact strict render={
+            () => {
+              return (
+                <Testrespo />
+              );
+            }
+          }/>
+          <Route path='/course' exact strict render={
+            () => {
+              return (
+                <Codemirror />
+              );
+            }
+          }/>
+          <Footer />
+
+        </div>
+      </Router>
     );
 
     }
